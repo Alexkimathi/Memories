09f6573225a39159c4c2baee2f60516019b3b18e
alex kimathi <keemalekie@gmail.com>
2022-10-13 23:51:44 +0300

    uploading

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "cards#index"
+
   
 
   resources :cards, only: [:index, :show, :create, :update, :destroy]
